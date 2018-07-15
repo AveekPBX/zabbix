48527d678894ed8aa8e85bb1763eb5e22dd04838
AveekPBX <41271420+AveekPBX@users.noreply.github.com>
2018-07-16 03:30:13 +0530

    Update zabbix.pl

--- zabbix/zabbix.pl
+++ zabbix/zabbix.pl
@@ -21,7 +21,7 @@ print "File Doesnt Exist\n";
 exit;
 }
 system "chmod 777 /etc/zabbix/zabbix-agentd.conf";
-print "Zabbix Server=\n";
+print "Zabbix Server:";
 my $zabbixserver=<STDIN>;
 system "sed -i s/Server=127.0.0.1/Server=\$zabbixserver/g /etc/zabbix/zabbix-agentd.conf";
 print "Enter Hostname: ";
